3956f041d8aee2dfd364d0d721256e05c7d7f8aa
Moonki Choi <52124204+iknoom@users.noreply.github.com>
2021-04-06 04:45:17 +0900

    @Primary 삭제

---
+++ iknoom/자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,35 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // code
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("Hello");
+            em.persist(member);
+//            Member findMember = em.find(Member.class, 1L);
+//            em.remove(findMember);
+//            findMember.setName("HelloJPA");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
