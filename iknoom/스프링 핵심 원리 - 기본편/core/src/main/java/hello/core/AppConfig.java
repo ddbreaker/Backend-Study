c8fd12f91efcb63ca4b397538f0d9fb28e64645e
Moonki Choi <52124204+iknoom@users.noreply.github.com>
2021-04-06 04:44:48 +0900

    오타 수정

--- iknoom/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
+++ iknoom/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -17,17 +16,17 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService () {
-        return new MemberServiceImpl(MemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public MemberRepository MemberRepository() {
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(MemberRepository(), discountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
